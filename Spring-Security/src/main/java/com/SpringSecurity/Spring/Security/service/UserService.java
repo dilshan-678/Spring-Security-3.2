6facf9a7daac80215c5262fd9d6aa8748d0a485b
DILSHAN PATHIRAGE <danidutharuka55@gmail.com>
2024-08-27 23:41:32 +0530

    set security configuration

---
+++ Spring-Security/src/main/java/com/SpringSecurity/Spring/Security/service/UserService.java
@@ -0,0 +1,9 @@
+package com.SpringSecurity.Spring.Security.service;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+
+    UserDetailsService userDetailsService();
+}
